f2a46c7be820416d8a809bf0b25997d49ef3cda8
Md. Shaiful Islam <islambuet@gmail.com>
2018-02-14 19:55:34 +0600

    Transfer Current Stock Updated

--- application/controllers/Transfer.php
+++ application/controllers/Transfer.php
@@ -501,24 +501,62 @@ class Transfer extends CI_Controller
     {
         $source_tables=array(
             'table_stockin_varieties'=>'arm_ems.ems_stockin_varieties',
+            'table_varieties'=>'arm_ems.ems_varieties',
+            'table_crop_types'=>'arm_ems.ems_crop_types',
             'table_stockin_excess_inventory'=>'arm_ems.ems_stockin_excess_inventory',
             'table_stockout'=>'arm_ems.ems_stockout',
             'table_sales_po'=>'arm_ems.ems_sales_po',
             'table_sales_po_details'=>'arm_ems.ems_sales_po_details',
-            'table_login_setup_classification_pack_size'=>'arm_login_2018_19.login_setup_classification_variety_pack_size'
+            'table_login_setup_classification_pack_size'=>'arm_login_2018_19.login_setup_classification_pack_size'
         );
         $destination_tables=array(
             'table_sms_stock_in_variety'=>'arm_sms_2018_19.sms_stock_in_variety',
             'table_sms_stock_in_variety_details'=>'arm_sms_2018_19.sms_stock_in_variety_details',
             'table_sms_stock_summary_variety'=>'arm_sms_2018_19.sms_stock_summary_variety'
         );
+        $crop_warehouse[1]=3;
+        $crop_warehouse[2]=3;
+        $crop_warehouse[3]=3;
+        $crop_warehouse[4]=3;
+        $crop_warehouse[5]=3;
+        $crop_warehouse[6]=3;
+        $crop_warehouse[7]=3;//
+        $crop_warehouse[8]=3;//
+        $crop_warehouse[9]=3;
+        $crop_warehouse[10]=3;
+        $crop_warehouse[11]=3;
+        $crop_warehouse[12]=3;
+        $crop_warehouse[26]=3;//
+        $crop_warehouse[27]=3;
+        $crop_warehouse[28]=3;
+        $crop_warehouse[29]=3;//
+        $crop_warehouse[30]=3;//
+
+        $crop_warehouse[13]=2;
+        $crop_warehouse[14]=2;
+        $crop_warehouse[15]=2;
+        $crop_warehouse[16]=2;
+        $crop_warehouse[17]=2;
+        $crop_warehouse[18]=2;
+        $crop_warehouse[19]=2;
+        $crop_warehouse[20]=2;
+        $crop_warehouse[23]=2;
+        $crop_warehouse[25]=2;
+
+        $crop_warehouse[21]=1;
+        $crop_warehouse[22]=1;
+        $crop_warehouse[24]=1;
+
+
 
         $stocks=array();
         //stock in
         $this->db->from($source_tables['table_stockin_varieties'].' stv');
         $this->db->select('stv.variety_id,stv.pack_size_id');
         $this->db->select('SUM(stv.quantity) stock_in');
-
+        $this->db->join($source_tables['table_varieties'].' v','v.id =stv.variety_id','INNER');
+        $this->db->join($source_tables['table_crop_types'].' crop_type','crop_type.id =v.crop_type_id','INNER');
+        $this->db->select('crop_type.crop_id crop_id');
         $this->db->group_by(array('stv.variety_id','stv.pack_size_id'));
 
 
@@ -529,6 +567,7 @@ class Transfer extends CI_Controller
 
         foreach($results as $result)
         {
+            $stocks[$result['variety_id']][$result['pack_size_id']]['crop_id']=$result['crop_id'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['stock_in']=$result['stock_in'];
             $stocks[$result['variety_id']][$result['pack_size_id']]['excess']=0;
             $stocks[$result['variety_id']][$result['pack_size_id']]['stock_out']=0;
@@ -604,7 +643,7 @@ class Transfer extends CI_Controller
                 $data['stock_in_id']=1;
                 $data['variety_id']=$variety_id;
                 $data['pack_size_id']=$pack_size_id;
-                $data['warehouse_id']=1;//will come from setup
+                $data['warehouse_id']=$crop_warehouse[$stock['crop_id']];
                 $data['quantity']=$stock['stock_in']+$stock['excess']-$stock['stock_out']-$stock['sales']+$stock['outlet_total'];
                 $data['revision']=1;
                 $data['date_created']=$time;
@@ -620,7 +659,7 @@ class Transfer extends CI_Controller
                 $data=array();
                 $data['variety_id']=$variety_id;
                 $data['pack_size_id']=$pack_size_id;
-                $data['warehouse_id']=1;//will come from setup
+                $data['warehouse_id']=$crop_warehouse[$stock['crop_id']];
                 $data['in_stock']=$stock['stock_in']+$stock['excess']-$stock['stock_out']-$stock['sales']+$stock['outlet_total'];
                 //$data['out_transfer_outlet']=$stock['outlet_total'];
                 $data['current_stock']=$stock['stock_in']+$stock['excess']-$stock['stock_out']-$stock['sales'];
