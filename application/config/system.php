297260eb783f550ac6080519f50f4eb1f90a2dce
Md. Shaiful Islam <islambuet@gmail.com>
2018-10-05 12:47:46 +0600

    205 changed to new ip

--- application/config/system.php
+++ application/config/system.php
@@ -5,7 +5,7 @@ $config['system_max_actions']=8;
 
 $config['system_site_root_folder']='login_2018_19';
 $config['system_upload_image_auth_key']='ems_2018_19';
-$config['system_upload_api_url']='http://180.234.223.205/api_file_server/upload';
+$config['system_upload_api_url']='http://45.251.59.5/api_file_server/upload';
 
 $config['system_status_yes']='Yes';
 $config['system_status_no']='No';
@@ -22,8 +22,8 @@ $config['system_status_delivered']='Delivered';
 $config['system_status_received']='Received';
 $config['system_status_rejected']='Rejected';
 
-$config['system_base_url_profile_picture']='http://180.234.223.205/login_2018_19/';
-$config['system_base_url_picture']='http://180.234.223.205/login_2018_19/';
+$config['system_base_url_profile_picture']='http://45.251.59.5/login_2018_19/';
+$config['system_base_url_picture']='http://45.251.59.5/login_2018_19/';
 
 $config['USER_TYPE_EMPLOYEE']=1;
 
