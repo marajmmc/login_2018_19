be03c7187911ddc3f1b45acb2c0f3aa6f73c94e5
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2018-10-11 10:43:05 +0600

    re-arrange system triggers js

--- js/system_triggers.js
+++ js/system_triggers.js
@@ -9,81 +9,63 @@ function system_preset(params={})
 
 function system_off_events()
 {
-    /*Common*/
-    $(document).off('change','#warehouse_id');
-    $(document).off('change','#warehouse_id_source');
-    $(document).off('change','#warehouse_id_destination');
+    /*Product */
     $(document).off('change','#crop_id');
-    $(document).off('change','#crop_type_id');
-    $(document).off('change','#variety_id');
-    $(document).off('change','#pack_size_id');
-    $(document).off("change","#fiscal_year_id");
-
-    $(document).off("change",".warehouse_id");
-    $(document).off('change','.warehouse_id_source');
-    $(document).off('change','.warehouse_id_destination');
     $(document).off("change",".crop_id");
+    $(document).off('change','#crop_type_id');
     $(document).off("change",".crop_type_id");
+    $(document).off('change','#variety_id');
     $(document).off("change",".variety_id");
+    $(document).off('change','#pack_size_id');
     $(document).off("change",".pack_size_id");
 
-    $(document).off("change","#items_container .crop_id");
-    $(document).off("change","#items_container .crop_type_id");
-    $(document).off('change','#items_container .variety_id');
-    $(document).off('change','#items_container .pack_size_id');
+    /*warehouse*/
+    $(document).off('change','#warehouse_id');
+    $(document).off("change",".warehouse_id");
+    $(document).off('change','#warehouse_id_source');
+    $(document).off('change','.warehouse_id_source');
+    $(document).off('change','#warehouse_id_destination');
+    $(document).off('change','.warehouse_id_destination');
+    
+    /*Date/Time/Year*/
+    $(document).off("change","#fiscal_year_id");
+    $(document).off('change', '.datepicker');
 
+    /*Location*/
     $(document).off('change', '#division_id');
-    $(document).off('change', '#zone_id');
-    $(document).off('change', '#territory_id');
-    $(document).off('change', '#district_id');
-
     $(document).off('change', '.division_id');
+    $(document).off('change', '#zone_id');
     $(document).off('change', '.zone_id');
+    $(document).off('change', '#territory_id');
     $(document).off('change', '.territory_id');
+    $(document).off('change', '#district_id');
     $(document).off('change', '.district_id');
 
+    /*Outlet*/
     $(document).off('change', '#customer_id');
-
     $(document).off('change', '#outlet_id');
     $(document).off('change', '#outlet_id_source');
     $(document).off('change', '#outlet_id_destination');
+    $(document).off('change', '#dealer_id');
 
+    /*Add More & Pop Up*/
     $(document).off("click", ".system_button_add_more");
     $(document).off('click','.system_button_add_delete');
     $(document).off("click", ".pop_up");
 
-    $(document).off('change','#purpose');
-
+    /*Module & Task*/
     $(document).off("click", ".task_action_all");
     $(document).off("click", ".task_header_all");
 
+    /*Quantity/Amout/Number*/
     $(document).off('input','.amount');
+    $(document).off('input', '.quantity_approve');
+    $(document).off('input', '.quantity_request');
 
-    $(document).off('input', '#items_container .quantity_approve');
-    $(document).off('input', '#items_container .quantity_request');
-
+    /*jqx grid*/
     $(document).off('click', '#button_action_save_jqx');
 
-    $(document).off('change', '.datepicker');
-    $(document).off('change', '#dealer_id');
-
-    /*SMS */
-    /*$(document).off('input','#quantity_convert');
-    $(document).off('input','#quantity_packet_actual');
-    $(document).off('input','#price_complete_other_variety_taka');
-    $(document).off('change','#principal_id');
-    $(document).off('input','#items_container .quantity_open');
-    $(document).off('change','#items_container .price_unit_currency');
-    $(document).off('input','#price_open_other_currency');
-    $(document).off('input','.quantity_receive');
-    $(document).off('input','#quantity_receive');
-    $(document).off('input','.quantity_release');
-    $(document).off('input','#price_release_other_currency');
-    $(document).off('change','#supplier_id');
-    $(document).off('input','#price_unit_tk');
-    $(document).off('input','.number_of_reel');
-    $(document).off('input','.quantity_supply');
-    $(document).off('change','.price_unit_tk');
-    $(document).off('change','#packing_item');*/
+    /*Others*/
+    $(document).off('change','#purpose');
 
 }
