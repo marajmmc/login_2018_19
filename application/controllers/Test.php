6b36f814089b17b809ad002d629402fa146788e7
Md. Shaiful Islam <islambuet@gmail.com>
2018-11-26 04:11:59 +0600

    Removed risky codes

--- application/controllers/Test.php
+++ application/controllers/Test.php
@@ -3,52 +3,8 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Test extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
     public function index()
     {
-        //$cookie_expire_time=time()+864000;
-        //echo $cookie_expire_time;
-        //set_cookie(User_helper::$mobile_verification_cookie_prefix.$verification_info['user_id'],$verification_info['id'],$cookie_expire_time);
-        //set_cookie('test',1,864000);
-        //setcookie("test", 1, $cookie_expire_time,'/');
 
     }
-	private function user_order()
-	{
-        $user = User_helper::get_user();
-
-        $this->db->from($this->config->item('table_setup_user').' user');
-        $this->db->select('user.employee_id,user.user_name');
-        $this->db->select('user_info.user_id,user_info.id');
-        $this->db->join($this->config->item('table_setup_user_info').' user_info','user.id = user_info.user_id','INNER');
-
-        $this->db->where('user_info.revision',1);
-        $results=$this->db->get()->result_array();
-        foreach($results as $result)
-        {
-            $data=array();
-            $data['ordering']=intval($result['employee_id']);
-            $this->db->where('id',$result['id']);
-            $this->db->update($this->config->item('table_setup_user_info'), $data);
-            echo '<PRE>';
-            print_r($data);
-            print_r($result['employee_id']);
-            echo '</PRE>';
-        }
-
-	}
 }
