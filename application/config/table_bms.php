e251f5d6f562db39c2010925463513225c908b7d
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2018-11-25 09:26:21 +0600

    new table added for bms

--- application/config/table_bms.php
+++ application/config/table_bms.php
@@ -7,3 +7,4 @@ $config['table_bms_di_budget_target'] = 'arm_bms_2018_19.bms_di_budget_target';
 $config['table_bms_di_budget_target_division'] = 'arm_bms_2018_19.bms_di_budget_target_division';
 $config['table_bms_hom_budget_target'] = 'arm_bms_2018_19.bms_hom_budget_target';
 $config['table_bms_hom_budget_target_hom'] = 'arm_bms_2018_19.bms_hom_budget_target_hom';
+$config['table_bms_setup_variety_pricing_budget'] = 'arm_bms_2018_19.bms_setup_variety_pricing_budget';
