7a802d9a10861f572d08436296dfd15d687ce7ff
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2019-04-01 13:24:52 +0600

    new config item added supplier

--- application/config/table_ams.php
+++ application/config/table_ams.php
@@ -2,3 +2,4 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['table_ams_setup_categories'] = 'arm_ams_2019_20.ams_setup_categories';
+$config['table_ams_setup_suppliers'] = 'arm_ams_2019_20.ams_setup_suppliers';
