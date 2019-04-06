24798bba08d2d3b5bba7af27fe6e2df7a1053a6f
mahmud369 <mahmud369@gmail.com>
2019-04-06 11:06:19 +0600

    1 config Added:

--- application/config/table_ams.php
+++ application/config/table_ams.php
@@ -2,4 +2,5 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['table_ams_setup_categories'] = 'arm_ams_2019_20.ams_setup_categories';
+$config['table_ams_setup_locations'] = 'arm_ams_2019_20.ams_setup_locations';
 $config['table_ams_setup_suppliers'] = 'arm_ams_2019_20.ams_setup_suppliers';
