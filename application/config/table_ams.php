48c9f8d2ee49b9a4176453c71adbff9fc49d767a
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2019-04-08 13:29:23 +0600

    New table config item added.

--- application/config/table_ams.php
+++ application/config/table_ams.php
@@ -4,3 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['table_ams_setup_categories'] = 'arm_ams_2019_20.ams_setup_categories';
 $config['table_ams_setup_locations'] = 'arm_ams_2019_20.ams_setup_locations';
 $config['table_ams_setup_suppliers'] = 'arm_ams_2019_20.ams_setup_suppliers';
+$config['table_ams_requisition_request'] = 'arm_ams_2019_20.ams_requisition_request';
