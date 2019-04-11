6a2faf212e58e10da9b858304619273281fe3377
Md. Maraj Hossain Tarafder <marajmmc@gmail.com>
2019-04-11 15:44:09 +0600

    table ams new config added

--- application/config/table_ams.php
+++ application/config/table_ams.php
@@ -5,3 +5,4 @@ $config['table_ams_setup_categories'] = 'arm_ams_2019_20.ams_setup_categories';
 $config['table_ams_setup_locations'] = 'arm_ams_2019_20.ams_setup_locations';
 $config['table_ams_setup_suppliers'] = 'arm_ams_2019_20.ams_setup_suppliers';
 $config['table_ams_requisition_request'] = 'arm_ams_2019_20.ams_requisition_request';
+$config['table_ams_requisition_payment'] = 'arm_ams_2019_20.ams_requisition_payment';
