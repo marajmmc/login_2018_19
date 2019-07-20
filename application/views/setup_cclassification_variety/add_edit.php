11d6ff3042095b5c98d3696634af9f5d26f5da7b
Md. Shaiful Islam <islambuet@gmail.com>
2019-07-21 04:55:31 +0600

    Disabled Competitor variety Create option

--- application/views/setup_cclassification_variety/add_edit.php
+++ application/views/setup_cclassification_variety/add_edit.php
@@ -77,11 +77,11 @@ $CI->load->view('action_buttons',array('action_buttons'=>$action_buttons));
             <div class="col-xs-4">
                 <label class="control-label pull-right"><?php echo $this->lang->line('LABEL_WHOSE');?><span style="color:#FF0000">*</span></label>
             </div>
-            <div class="col-sm-4 col-xs-8">
+            <div class="col-sm-4 col-xs-8"">
                 <div class="radio-inline">
                     <label><input type="radio" value="ARM" <?php if($item['whose']=='ARM'){echo 'checked';} ?> name="item[whose]">ARM</label>
                 </div>
-                <div class="radio-inline">
+                <div class="radio-inline" style="<?php if($item['id']==0){echo 'display:none';} ?>">
                     <label><input type="radio" value="Competitor" <?php if($item['whose']=='Competitor'){echo 'checked';} ?> name="item[whose]">Competitor</label>
                 </div>
                 <div class="radio-inline">
