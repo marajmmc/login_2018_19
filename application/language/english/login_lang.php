6e998c62a620e3b5821cbbf5c5c13efd09412bae
Md. Shaiful Islam <islambuet@gmail.com>
2019-08-29 11:30:37 +0600

    Label Changed Division and zone

--- application/language/english/login_lang.php
+++ application/language/english/login_lang.php
@@ -156,8 +156,8 @@ $lang['LABEL_USER_AREA'] = 'Location';
 
 //location setup
 //Create zone
-$lang['LABEL_DIVISION_NAME'] = 'Division';
-$lang['LABEL_ZONE_NAME'] = 'Zone';
+$lang['LABEL_DIVISION_NAME'] = 'Region';
+$lang['LABEL_ZONE_NAME'] = 'Area';
 $lang['LABEL_TERRITORY_NAME'] = 'Territory';
 $lang['LABEL_DISTRICT_NAME'] = 'District';
 $lang['LABEL_UPAZILLA_NAME'] = 'Upazila';
