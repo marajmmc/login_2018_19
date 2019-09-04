0d967bd9f27c213b686c48041b9fbf5240d228f0
mahmud369 <mahmud369@gmail.com>
2019-09-04 13:14:17 +0600

    table_bi UPDATE

--- application/config/table_bi.php
+++ application/config/table_bi.php
@@ -1,7 +1,12 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
+// Setup Tables
 $config['table_bi_setup_competitor_variety'] = 'arm_bi_2019_20.bi_setup_competitor_variety';
 $config['table_bi_setup_competitor_variety_characteristics'] = 'arm_bi_2019_20.bi_setup_competitor_variety_characteristics';
 $config['table_bi_setup_competitor_variety_files'] = 'arm_bi_2019_20.bi_setup_competitor_variety_files';
 $config['table_bi_setup_arm_variety_comparison'] = 'arm_bi_2019_20.bi_setup_arm_variety_comparison';
+
+// Market Size Tables
+$config['table_bi_market_size_request'] = 'arm_bi_2019_20.bi_market_size_request';
+$config['table_bi_market_size_main'] = 'arm_bi_2019_20.bi_market_size_main';
