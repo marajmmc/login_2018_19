ec756354d8adaad13b1642f003f73f7f96ecc490
mahmud369 <mahmud369@gmail.com>
2019-09-25 13:36:23 +0600

    Table for Bi > Major Competitor variety added

--- application/config/table_bi.php
+++ application/config/table_bi.php
@@ -9,7 +9,12 @@ $config['table_bi_setup_arm_variety_comparison'] = 'arm_bi_2019_20.bi_setup_arm_
 
 // Market Size Tables
 $config['table_bi_market_size_request'] = 'arm_bi_2019_20.bi_market_size_request';
-$config['table_bi_market_size_main'] = 'arm_bi_2019_20.bi_market_size_main';
+$config['table_bi_market_size'] = 'arm_bi_2019_20.bi_market_size';
+
 // Variety Cultivation Period
-$config['table_bi_variety_cultivation_period'] = 'arm_bi_2019_20.bi_variety_cultivation_period';
 $config['table_bi_variety_cultivation_period_request'] = 'arm_bi_2019_20.bi_variety_cultivation_period_request';
+$config['table_bi_variety_cultivation_period'] = 'arm_bi_2019_20.bi_variety_cultivation_period';
+
+// Major Competitor Variety
+$config['table_bi_major_competitor_variety_request'] = 'arm_bi_2019_20.bi_major_competitor_variety_request';
+$config['table_bi_major_competitor_variety'] = 'arm_bi_2019_20.bi_major_competitor_variety';
