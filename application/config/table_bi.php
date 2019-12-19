f87c506cca2ae5559f7348a1c54a1f5e0952b446
mahmud369 <mahmud369@gmail.com>
2019-12-19 13:31:13 +0600

    Bi Table Config File Updated -> 19-DEC-19

--- application/config/table_bi.php
+++ application/config/table_bi.php
@@ -5,20 +5,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['table_bi_setup_competitor_variety'] = 'arm_bi_2019_20.bi_setup_competitor_variety';
 $config['table_bi_setup_competitor_variety_characteristics'] = 'arm_bi_2019_20.bi_setup_competitor_variety_characteristics';
 $config['table_bi_setup_competitor_variety_files'] = 'arm_bi_2019_20.bi_setup_competitor_variety_files';
-$config['table_bi_setup_arm_variety_comparison'] = 'arm_bi_2019_20.bi_setup_arm_variety_comparison';
+$config['table_bi_setup_variety_focused'] = 'arm_bi_2019_20.bi_setup_variety_focused';
+$config['table_bi_setup_variety_cultivation_period'] = 'arm_bi_2019_20.bi_setup_variety_cultivation_period';
+$config['table_bi_setup_season'] = 'arm_bi_2019_20.bi_setup_season';
 
+/* ------------- Variety Info ------------- */
 // Market Size Tables
 $config['table_bi_market_size_request'] = 'arm_bi_2019_20.bi_market_size_request';
 $config['table_bi_market_size'] = 'arm_bi_2019_20.bi_market_size';
 
-// Variety Cultivation Period
-$config['table_bi_setup_variety_cultivation_period'] = 'arm_bi_2019_20.bi_setup_variety_cultivation_period';
-$config['table_bi_variety_cultivation_period'] = 'arm_bi_2019_20.bi_variety_cultivation_period';
+// Variety Cultivation Period Tables
 $config['table_bi_variety_cultivation_period_request'] = 'arm_bi_2019_20.bi_variety_cultivation_period_request';
-// Major Competitor Variety
-$config['table_bi_setup_variety_major_competitor'] = 'arm_bi_2019_20.bi_setup_variety_major_competitor';
-$config['table_bi_major_competitor_variety_request'] = 'arm_bi_2019_20.bi_major_competitor_variety_request';
-$config['table_bi_major_competitor_variety'] = 'arm_bi_2019_20.bi_major_competitor_variety';
+$config['table_bi_variety_cultivation_period'] = 'arm_bi_2019_20.bi_variety_cultivation_period';
+
+// Crop Type Preference Tables
+$config['table_bi_crop_type_preference'] = 'arm_bi_2019_20.bi_crop_type_preference';
+
 // Upazilla wise ARM Variety
 $config['table_bi_variety_arm_upazilla_request'] = 'arm_bi_2019_20.bi_variety_arm_upazilla_request';
 $config['table_bi_variety_arm_upazilla'] = 'arm_bi_2019_20.bi_variety_arm_upazilla';
+
+// Major Competitor Variety
+$config['table_bi_major_competitor_variety_request'] = 'arm_bi_2019_20_dev.bi_major_competitor_variety_request';
+$config['table_bi_major_competitor_variety'] = 'arm_bi_2019_20_dev.bi_major_competitor_variety';
