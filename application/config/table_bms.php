3b1dbf952c3d6154f5664320f7e67e3da8eb7186
mahmud369 <mahmud369@gmail.com>
2020-02-13 15:15:05 +0600

    4 Tables for Target tasks, added.

--- application/config/table_bms.php
+++ application/config/table_bms.php
@@ -13,3 +13,8 @@ $config['table_bms_setup_budget_config_variety_pricing'] = 'arm_bms_2018_19.bms_
 $config['table_bms_principal_quantity'] = 'arm_bms_2018_19.bms_principal_quantity';
 $config['table_bms_principal_quantity_principal'] = 'arm_bms_2018_19.bms_principal_quantity_principal';
 $config['table_bms_budget_variety_pricing_finalize'] = 'arm_bms_2018_19.bms_budget_variety_pricing_finalize';
+// Target Tables
+$config['table_bi_target_hosm'] = 'arm_bi_2019_20.bi_target_hosm';
+$config['table_bi_target_dsm'] = 'arm_bi_2019_20.bi_target_dsm';
+$config['table_bi_target_ams'] = 'arm_bi_2019_20.bi_target_ams';
+$config['table_bi_target_tsme'] = 'arm_bi_2019_20.bi_target_tsme';
